7797f83e7e602d91e3a662a77120964da900e198
Maik Schöneich <macgyverx83@gmail.com>
2019-08-04 09:06:29 +0200

    chore(frontend): integrate client into build pipeline

--- src/backend/CentFuxx.Products/Startup.cs
+++ src/backend/CentFuxx.Products/Startup.cs
@@ -1,15 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.IO;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 
 namespace CentFuxx.Products
 {
@@ -43,6 +37,26 @@ namespace CentFuxx.Products
 
             app.UseHttpsRedirection();
             app.UseMvc();
+
+            // Host static files in wwwroot folder
+            var defaultFileOptions = new DefaultFilesOptions();
+            defaultFileOptions.DefaultFileNames.Clear();
+            defaultFileOptions.DefaultFileNames.Add("/index.html");
+            app.UseDefaultFiles(defaultFileOptions);
+            app.UseStaticFiles();
+
+            // Fallback for URL routing in Angular application
+            app.Use(async (context, next) =>
+            {
+                await next();
+                if (context.Response.StatusCode == 404 
+                    && !Path.HasExtension(context.Request.Path.Value))
+                {
+                    context.Request.Path = "/index.html";
+                    context.Response.StatusCode = 200;
+                    await next();
+                }
+            });
         }
     }
 }
