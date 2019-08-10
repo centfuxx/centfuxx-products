6d5ad2e0562a10106415e531c5d124f9e49d051b
Maik Schöneich <macgyverx83@gmail.com>
2019-08-11 01:20:59 +0200

    feat(routing): use lazy loading for products module

--- src/frontend/src/app/products/products.module.ts
+++ src/frontend/src/app/products/products.module.ts
@@ -5,10 +5,36 @@ import { MatCardModule } from "@angular/material/card";
 import { MatButtonModule } from "@angular/material/button";
 import { ProductListComponent } from "./product-list/product-list.component";
 import { ProductCardComponent } from "./product-card/product-card.component";
+import { AddProductComponent } from "./pages/add-product/add-product.component";
+import { RouterModule, Routes } from "@angular/router";
+import { ProductsComponent } from "./components/products/products.component";
+
+const routes: Routes = [
+    {
+        path: "",
+        component: ProductsComponent,
+        children: [
+            { path: "list", component: ProductListComponent },
+            { path: "add", component: AddProductComponent },
+            { path: "", redirectTo: "list", pathMatch: "full" }
+        ]
+    }
+];
 
 @NgModule({
-    declarations: [ProductListComponent, ProductCardComponent],
+    declarations: [
+        ProductListComponent,
+        ProductCardComponent,
+        AddProductComponent,
+        ProductsComponent
+    ],
     exports: [ProductListComponent],
-    imports: [CommonModule, HttpClientModule, MatCardModule, MatButtonModule]
+    imports: [
+        CommonModule,
+        HttpClientModule,
+        MatCardModule,
+        MatButtonModule,
+        RouterModule.forChild(routes)
+    ]
 })
 export class ProductsModule {}
