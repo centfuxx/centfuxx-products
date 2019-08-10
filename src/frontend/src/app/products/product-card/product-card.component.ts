9d0068a891414d233b0ae74b4c88939778d422b5
Maik Schöneich <macgyverx83@gmail.com>
2019-08-10 12:21:11 +0200

    feat(product-card): provide product card for display in a list

---
+++ src/frontend/src/app/products/product-card/product-card.component.ts
@@ -0,0 +1,15 @@
+import { Product } from "./../models/product";
+import { Component, OnInit, Input } from "@angular/core";
+
+@Component({
+    selector: "app-product-card",
+    templateUrl: "./product-card.component.html",
+    styleUrls: ["./product-card.component.scss"]
+})
+export class ProductCardComponent implements OnInit {
+    @Input() public product: Product;
+
+    constructor() {}
+
+    ngOnInit() {}
+}
--- src/frontend/src/app/products/products.module.ts
+++ src/frontend/src/app/products/products.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { HttpClientModule } from "@angular/common/http";
-import { ProductListComponent } from "./product-list/product-list.component";
 import { MatCardModule } from "@angular/material/card";
 import { MatButtonModule } from "@angular/material/button";
+import { ProductListComponent } from "./product-list/product-list.component";
+import { ProductCardComponent } from "./product-card/product-card.component";
 
 @NgModule({
-    declarations: [ProductListComponent],
+    declarations: [ProductListComponent, ProductCardComponent],
     exports: [ProductListComponent],
     imports: [CommonModule, HttpClientModule, MatCardModule, MatButtonModule]
 })
