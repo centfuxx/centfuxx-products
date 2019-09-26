8d49e028cdd764a78e598cabd6d413e463398098
Maik Schöneich <macgyverx83@gmail.com>
2019-09-26 21:43:04 +0200

    chore: serve angular app with ssl support

--- src/backend/CentFuxx.Products.Storage.EfCore.MySql/MySqlProductContext.cs
+++ src/backend/CentFuxx.Products.Storage.EfCore.MySql/MySqlProductContext.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Text;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Options;
+﻿using Microsoft.EntityFrameworkCore;
 
 namespace CentFuxx.Products.Storage.EfCore.MySql
 {
